0915217f77521a8f3ab9b0de81810118308f3fd5
Quantum Shift <binayakbytes@gmail.com>
2025-04-02 19:08:29 +0545

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,15 @@
 // EmailJS Configuration
 // const config = {
 //     emailjs: {
-//         SERVICE_ID: 'service_qp9wp0k',
-//         TEMPLATE_ID: 'template_27bvmdr',
-//         PUBLIC_KEY: '1Jai0d1F_4TM_X9Bu'
+//         SERVICE_ID: 'Nonchalant',
+//         TEMPLATE_ID: 'Nonchalant',
+//         PUBLIC_KEY: 'Nonchalant'
 //     }
 // };
 
 // Add to window object to make it globally available
 window.appConfig = config; 
+?
+/
+>
+...
